3a0f0951789b6ab04cc604fb333f8aa4cf0bd1cd
progerWolf <programmerwolf8@gmail.com>
2021-08-26 18:24:35 +0500

    Added migrations, models, factories and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $tags = Tag::factory(10)->create();
+
+        $posts = Post::factory(10)->create();
+
+        $tagsId = $tags->pluck('id');
+
+        $posts->each(function ($post) use ($tagsId){
+            $post->tags()->attach($tagsId->random(3));
+        });
+
     }
 }
